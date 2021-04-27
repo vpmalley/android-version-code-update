535a1425fc763e8498298fb571c50209688b7d13
Vince <vpmalley@users.noreply.github.com>
2021-04-27 14:57:36 -0400

    [Google] Trying to update the version code

--- src/publish.ts
+++ src/publish.ts
@@ -15,25 +15,27 @@ export interface Options {
 }
 
 export async function fetchVersionCode(options: Options): Promise<Number> {
+  core.info(`Creating a new Edit for this app`)
   const appEdit = await androidPublisher.edits.insert({
-    packageName: options.applicationId,
-    auth: options.auth
-  })
+      auth: options.auth,
+      packageName: options.applicationId
+  });
 
+  core.info(`Fetching tracks details`)
   const tracks = await androidPublisher.edits.tracks.list({
-    auth: options.auth,
-    packageName: options.applicationId,
-    editId: appEdit.data.id!
-  })
+        auth: options.auth,
+        editId: appEdit.data.id!,
+        packageName: options.applicationId
+    });
 
   const versionCodes: number[] = []
-
   tracks.data.tracks?.forEach(track => {
     track.releases?.forEach(release => {
       versionCodes.push(Number(release.versionCodes![0]))
     })
   })
-
+  
+  core.info(`Found ${versionCodes.length} tracks`)
   if (versionCodes.length > 0) {
     return Math.max(...versionCodes)
   } else {
